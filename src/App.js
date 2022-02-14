4765503401c3cdee45b3aa6df4e6a253f93d186a
Mureed Hussain <imhussain0086@gmail.com>
2022-02-14 22:02:40 +0500

    SidebarMenu
    SidebarMenu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import SidebarMenu from "./SidebarMenu";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SidebarMenu />
     </div>
   );
 }
---
+++ src/SidebarMenu.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./SidebarMenu.css";
+
+const SidebarMenu = () => {
+  return (
+    <div className="sidebarmenu">
+      <a>Entry Forms</a>
+
+      <br />
+      <hr />
+      <a>Ledgers</a>
+      <br />
+      <hr />
+      <a>Stock Managment</a>
+      <br />
+      <hr />
+      <a>Warehouse Managment</a>
+      <br />
+      <hr />
+      <a>Sales Analysis</a>
+      <br />
+      <hr />
+      <a>Customer Financial</a>
+      <br />
+      <hr />
+      <a>Yealry Sales</a>
+      <br />
+      <hr />
+      <a>Cashier Desk</a>
+      <br />
+      <hr />
+      <a>Financial Reports</a>
+      <br />
+      <hr />
+      <a>Purchase Analysis</a>
+      <br />
+      <hr />
+      <a>Sales Order Dash Board</a>
+      <br />
+      <hr />
+      <a>Reports</a>
+      <br />
+      <hr />
+      <a>Admin</a>
+    </div>
+  );
+};
+
+export default SidebarMenu;
